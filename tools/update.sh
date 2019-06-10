331c71e0d67efcb31977473e3429d632b3fa0012
Alex Mills <alex@asdfx.us>
2019-06-10 13:20:44 +0100

    Update received from Greenbone Community Feed

--- tools/update.sh
+++ tools/update.sh
@@ -5,27 +5,39 @@ echo '';
 echo 'Downloading NVT feed';
 echo '';
 echo '###########################################################';
-wget -q http://dl.greenbone.net/community-nvt-feed-current.tar.bz2
+DOWNLOAD_COMMAND="wget -q http://dl.greenbone.net/community-nvt-feed-current.tar.bz2";
+echo $DOWNLOAD_COMMAND;
+$DOWNLOAD_COMMAND;
 
 echo '###########################################################';
 echo '';
 echo 'Extracting NVT feed';
 echo '';
 echo '###########################################################';
-tar -xjf community-nvt-feed-current.tar.bz2
+EXTRACT_COMMAND="tar -xjf community-nvt-feed-current.tar.bz2";
+echo $EXTRACT_COMMAND;
+$EXTRACT_COMMAND
 
 echo '###########################################################';
 echo '';
 echo 'Deleting tarball';
 echo '';
 echo '###########################################################';
-rm -rf community-nvt-feed-current.tar.bz2
+DELETE_COMMAND="rm -rf community-nvt-feed-current.tar.bz2"
+echo $DELETE_COMMAND;
+$DELETE_COMMAND
 
 echo '###########################################################';
 echo '';
 echo 'Adding updated NVTs to git, and pushing to repository';
 echo '';
 echo '###########################################################';
-git add .
-git commit -m "Update received from Greenbone Community feed";
-git push -q
+GIT_ADD_COMMAND="git add .";
+GIT_COMMIT_COMMAND="git commit -m \"Update received from Greenbone Community feed\"";
+GIT_PUSH_COMMAND="git push -q";
+echo $GIT_ADD_COMMAND;
+echo $GIT_COMMIT_COMMAND;
+echo $GIT_PUSH_COMMAND;
+$GIT_ADD_COMMAND;
+$GIT_COMMIT_COMMAND;
+$GIT_PUSH_COMMAND;
